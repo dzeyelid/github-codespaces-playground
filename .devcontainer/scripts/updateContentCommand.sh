25a683bd033d846df2ac0b0c770bdee16acb8a64
Kazumi OHIRA (@dz_) <dzeyelid@gmail.com>
2022-08-03 22:20:42 +0000

    Reconstruct app code

--- .devcontainer/scripts/updateContentCommand.sh
+++ .devcontainer/scripts/updateContentCommand.sh
@@ -3,14 +3,24 @@
 # updateContentCommand では、コンテンツが更新された増分ビルドの際に実行されます。
 # パッケージのインストールなどをしておくことで、立上げ後すぐに利用できます。
 
-if [ -d "app" ]; then
-pushd app
+if [ -d "frontend/app" ]; then
+pushd frontend/app
 npm install
 popd
 fi
 
-if [ -d "app/api" ]; then
-pushd app/api
+if [ -d "frontend/app/api" ]; then
+pushd frontend/app/api
+npm install
+npm run build
+if [ ! -e "local.settings.json" ]; then
+cp local.settings.development.json local.settings.json
+fi
+popd
+fi
+
+if [ -d "backend" ]; then
+pushd backend
 npm install
 npm run build
 if [ ! -e "local.settings.json" ]; then
