83af85013b9fdf3fd39491cf1365af07498f15e7
Kazumi OHIRA (@dz_) <dzeyelid@gmail.com>
2022-08-03 15:48:05 +0000

    Update dev container configuration

--- .devcontainer/scripts/updateContentCommand.sh
+++ .devcontainer/scripts/updateContentCommand.sh
@@ -2,8 +2,15 @@
 
 # updateContentCommand では、コンテンツが更新された増分ビルドの際に実行されます。
 # パッケージのインストールなどをしておくことで、立上げ後すぐに利用できます。
+
 if [ -d "app" ]; then
 pushd app
+npm install
+popd
+fi
+
+if [ -d "app/api" ]; then
+pushd app/api
 npm install
 popd
 fi
